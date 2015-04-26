e59b9dba9887add94713b0dea19bd4c3b526d4a1
J. Cohen <hostelmarketing@gmail.com>
2015-04-26 00:53:37 -0700

    Attach event listeners

--- static/js/app.js
+++ static/js/app.js
@@ -1,14 +1,18 @@
 // MVC experiments
 
 var model = {
+    init: function () {
+        // TODO: create models and put them in localStorage
+        console.log('Initialize models');
+    },
     getAllAnimals: function () {
         var data = {};
         data.animals = [
-            { 'name': 'Harry', 'counter': 0, 'picture': '/static/images/1.jpg' },
-            { 'name': 'Bob', 'counter': 0, 'picture': '/static/images/2.jpg' },
-            { 'name': 'Bambi', 'counter': 0, 'picture': '/static/images/3.jpg' },
-            { 'name': 'Clawz', 'counter': 0, 'picture': '/static/images/4.jpg' },
-            { 'name': 'Curly', 'counter': 0, 'picture': '/static/images/5.jpg' }
+            { 'id': 1, 'name': 'Harry', 'counter': 0, 'picture': '/static/images/1.jpg' },
+            { 'id': 2, 'name': 'Bob', 'counter': 0, 'picture': '/static/images/2.jpg' },
+            { 'id': 3, 'name': 'Bambi', 'counter': 0, 'picture': '/static/images/3.jpg' },
+            { 'id': 4, 'name': 'Clawz', 'counter': 0, 'picture': '/static/images/4.jpg' },
+            { 'id': 5, 'name': 'Curly', 'counter': 0, 'picture': '/static/images/5.jpg' }
         ];
         return data;
     }
@@ -16,27 +20,44 @@ var model = {
 
 var controller = {
     init: function () {
-        console.log('init');
+        model.init();
+        animalListView.init();
     },
     getAnimals: function () {
         return model.getAllAnimals();
     }
 };
 
+// TODO: use a view Class
 var animalListView = {
     init: function () {
         this.animalList = $('#animalList');
         animalListView.render();
     },
     render: function () {
-        var outputHtml = '';
-        controller.getAnimals().forEach(function (animal) {
+        var outputHtml = '',
+            animals,
+            animalLinks,
+            i, iLen;
+
+        animals = controller.getAnimals().animals;
+        animals.forEach(function (animal) {
             outputHtml += '<li class="list-group-item"><span class="badge">' +
                         animal.counter + '</span>' +
-                        '<a href="#" class="animalListLink">' +
+                        '<a href="#" class="animalListLink" data-animal-id="' +
+                        animal.id + '">' +
                         animal.name + '</a>' + '</li>';
         });
         this.animalList.html(outputHtml);
+        animalLinks = document.querySelectorAll('.animalListLink');
+        for (i = 0, iLen = animalLinks.length - 1; i <= iLen; i ++) {
+            animalLinks[i].addEventListener('click', this.clickHandler, false);
+        }
+    },
+    clickHandler: function (e) {
+        e.preventDefault();
+        var animalId = this.dataset.animalId;
+        console.log(animalId);
     }
 };
 
